2c6a1439edcedc9bd0ce55d76d1fb770c02ba5bc
Mikheev Nikita <mishana32@gmail.com>
2021-09-12 17:12:50 +0500

    добавлены комментарии

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,9 @@ public class Main {
         String text = scanner.nextLine();
         text = text.toLowerCase();
         HashMap<Character, Integer> dictionary = new HashMap<>();
+        /*циклом перебираем введенный текст
+        * если буква уже есть в словаре, до заменяем ее в коллекции с
+        * увеличенным счетчиков, если такой буквы нет то добавляем её*/
         for (int i=0; i < text.length(); i++) {
             if(dictionary.containsKey(text.charAt(i))){
                 Integer temp = dictionary.get(text.charAt(i));
